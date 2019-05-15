979c605c8e28a60013e7afd5066ec7aa43eece49
Jarl-Erik Malmström <jarl-erik.malmstrom@wunderdog.fi>
2019-05-15 16:50:20 +0300

    New FE build

---
+++ build/precache-manifest.521fcfc1cc51e956e6566dcd19994090.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "332077f73b19714e8d2b",
+    "url": "/static/js/main.d2896e68.chunk.js"
+  },
+  {
+    "revision": "9beaa893eeb0ff78fc90",
+    "url": "/static/js/2.cbd13921.chunk.js"
+  },
+  {
+    "revision": "332077f73b19714e8d2b",
+    "url": "/static/css/main.6a84bc8b.chunk.css"
+  },
+  {
+    "revision": "aef632e882f135367d5d6d563736efa7",
+    "url": "/index.html"
+  }
+];
